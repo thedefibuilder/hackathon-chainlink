cdbb42fb9d8e64eb98e9d38456e05a55cb133ca0
urataps <alexeluca.spataru@gmail.com>
2024-05-23 22:23:34 +0300

    chore: install chainlink

--- contracts/src/AuditRegistry.sol
+++ contracts/src/AuditRegistry.sol
@@ -3,17 +3,29 @@ pragma solidity >=0.8.25;
 
 import { ERC1155 } from "@oz/token/ERC1155/ERC1155.sol";
 import { ERC1155URIStorage } from "@oz/token/ERC1155/extensions/ERC1155URIStorage.sol";
+import { FunctionsClient } from "@chainlink/functions/v1_0_0/FunctionsClient.sol";
+import { FunctionsRequest } from "@chainlink/functions/v1_0_0/libraries/FunctionsRequest.sol";
 
-contract AuditRegistry is ERC1155URIStorage {
+contract AuditRegistry is ERC1155URIStorage, FunctionsClient {
+    using FunctionsRequest for FunctionsRequest.Request;
+
     mapping(uint256 tokenId => string sourceCodeURI) public sourceCodeURIs;
 
-    constructor(string memory uri) ERC1155(uri) {
+    constructor(address functionsRouter) ERC1155("") FunctionsClient(functionsRouter) {
         // solhint-disable-previous-line no-empty-blocks
     }
 
-    function requestAudit(string memory sourceCodeURI) external {
+    function requestAudit(string memory sourceCodeURI) external returns (bytes32 requestId) {
         // solhint-disable-previous-line no-empty-blocks
 
         // send audit request
+    }
+
+    function fulfillRequest(bytes32 requestId, bytes memory response, bytes memory err) internal override {
+        // solhint-disable-previous-line no-empty-blocks
+
+        // process audit response
+
+        // mint token
     }
 }
