dcc1a66e5b805f0de5cd6990a53b1e6ab2609787
Abhi_Joshi <joshi.abhiram98@gmail.com>
2024-05-30 00:26:57 +0100

    Better data

--- auditor/main.py
+++ auditor/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from typing import Optional
 from manage_data import create_document, add_documents
+from generate import FunctionAuditReport, generate_function_audit
 from langchain_community.vectorstores import MongoDBAtlasVectorSearch
 from pymongo.errors import ConnectionFailure,OperationFailure,BulkWriteError
 from fastapi.exceptions import HTTPException
@@ -12,18 +14,18 @@ Module to define our API endpoints
 
 class CodeExample(BaseModel):
     code: str
-    detail:str
-    impact:str | None = None
-    likelihood:str | None = None
-    severity:str | None = None
-    recommendation:str | None = None
+    detail: str
+    impact: Optional[str] = None
+    likelihood: Optional[str] = None
+    severity: Optional[str] = None
+    recommendation: Optional[str] = None
 
 class CodeExampleList(BaseModel):
-    code_examples:list[CodeExample]
-    collection:str = "v1"
+    code_examples: list[CodeExample]
+    collection: str = "v1"
 
 class Query(BaseModel):
-    contract:str
+    function_code: str
 
 app = FastAPI()
 
@@ -31,7 +33,7 @@ app = FastAPI()
 @app.post("/add_code_examples/")
 async def add_code_examples(snippets:CodeExampleList):
     docs = []
-    for snippet in snippets:
+    for snippet in snippets.code_examples:
         docs.append(create_document(
             code=snippet.code,
             detail=snippet.detail,
@@ -50,6 +52,10 @@ async def add_code_examples(snippets:CodeExampleList):
                 namespace=f"{dbname}.{collection}"
             )
         )
+        # Search VectorDB for 
+
+
+
         return {
             "status": 201, 
             "detail": "Code examples were successfully added to vulnerbaility store"
@@ -68,6 +74,26 @@ async def add_code_examples(snippets:CodeExampleList):
 ## Query model
 ### We want to query the vector DB for similarity, then we will prompt the LLM 
 ### to detect vulnerabilities, then we will query to suggest recommendations
-
+@app.post('/audit_function/')
+async def audit_function(_function:Query):
+    try:
+        function_audit = generate_function_audit(_function.function_code)
+        return function_audit
+    
+    except ConnectionFailure as e:
+        raise HTTPException(
+            status_code=503,
+            detail=f"Connection Error: {e}"
+        )
+    except (BulkWriteError, OperationFailure) as e:
+        raise HTTPException(
+            status_code=500,
+            detail=f"There was an error adding code examples: {e}"
+        )
+    except Exception as e:
+        raise HTTPException(
+            status_code=500,
+            detail=str(e)
+        )
 
 ## TODO: Eventually we will add a training endpoint as well
